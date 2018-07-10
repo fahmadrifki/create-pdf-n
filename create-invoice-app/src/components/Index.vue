26bb0f285a87382b8084ca45c57230fb6994772f
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2018-07-10 09:37:53 +0000

    update

---
+++ create-invoice-app/src/components/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+      <div class="col-12">
+          <h1>{{ title }}</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Create Invoice',
+  data () {
+    return {
+      title: 'Create Invoice Nusanet'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+    color:#333;
+}
+</style>
---
+++ create-invoice-app/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Index from '@/components/Index'
+import HelloWorld from '@/components/HelloWorld'
+const NotFound = { template: '<p>Page not found</p>' }
+
+const routes = {
+  '/': Index,
+  '/hello': HelloWorld
+}
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
